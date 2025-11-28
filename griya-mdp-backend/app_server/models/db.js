e9775be0e3cebcefdfe6a3bfb294787eb866f4f6
ProHoky <aaryadarmasaputra@gmail.com>
2025-11-28 16:58:15 +0700

    register backend

--- griya-mdp-backend/app_server/models/db.js
+++ griya-mdp-backend/app_server/models/db.js
@@ -1,18 +1,22 @@
 let mongoose = require("mongoose");
-let dbURI = "mongodb://localhost:27017/PAWII-SI";
+let dbURI = "mongodb+srv://paw2:si@paw2.iendmj6.mongodb.net/PAWII-SI?retryWrites=true&w=majority&appName=paw2";
 
-mongoose.connect(dbURI, {
-  //useNewURLParser: true
+mongoose.connect(dbURI,{
+    //useNewURLParser: true
 });
 
 mongoose.connection.on("connected", () => {
-  console.log("Connected To MongoDB");
+    console.log("Connected To MongoDB");
 });
 
 mongoose.connection.on("error", (error) => {
-  console.log("Connection Error: " + error);
+    console.log("Connection Error: " + error);
 });
 
 mongoose.connection.on("disconected", () => {
-  console.log("Disconnected From MongoDB");
+    console.log("Disconnected From MongoDB");
 });
+
+// Load models
+require("./user");
+require("./housing");
